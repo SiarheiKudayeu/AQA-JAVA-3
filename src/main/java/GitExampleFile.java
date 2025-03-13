d929ebc31cc3cbd00d3f483c81956aa2ecda7580
s.kudaev <s.kudaev@andersenlab.com>
2025-03-13 19:23:22 +0300

    edit GitExampleFile file

--- src/main/java/GitExampleFile.java
+++ src/main/java/GitExampleFile.java
@@ -1,5 +1,8 @@
 public class GitExampleFile {
     public static void main(String[] args) {
         System.out.println("Hello AQA JAVA 3");
+
+        int x = 0;
+        System.out.println(++x);
     }
 }
